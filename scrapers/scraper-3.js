1f104f4e1545bc3936d2dd1f13785b1a06debbbf
Alexe <tatarualexe@yahoo.com>
2025-03-31 14:59:04 +0100

    Rewrote the third scraper to work on every city in the world.

--- scrapers/scraper-3.js
+++ scrapers/scraper-3.js
@@ -29,9 +29,9 @@ const cachePath = path.join(__dirname, "cache", cacheFile);
         console.log("Browser launched.");
 
         const page = await browser.newPage();
-        const url = `https://www.lonelyplanet.com/${country.toLowerCase()}/${city.toLowerCase()}/attractions`;
-        await page.goto(url, { waitUntil: "networkidle2" });
-        console.log("Navigated URL.");
+        const searchUrl = "https://www.lonelyplanet.com/search";
+        await page.goto(searchUrl, { waitUntil: "networkidle2" });
+        console.log("Navigated to search page.");
 
         try {
             await page.waitForSelector("#onetrust-accept-btn-handler", { timeout: 5000 });
@@ -41,7 +41,34 @@ const cachePath = path.join(__dirname, "cache", cacheFile);
             console.log("No cookie consent dialog found or could not interact with it.");
         }
 
-        await page.waitForSelector("a.card-link.line-clamp-2");
+        await page.evaluate(() => {
+            const button = document.querySelector('button[data-tagular*="ElementClicked"][data-tagular*="Search"]');
+            if (button) {
+                button.click();
+            }
+        });
+
+        const searchSelector = "input#searchInputComponent";
+        await page.waitForSelector(searchSelector, { visible: true, timeout: 10000 });
+        await page.focus(searchSelector);
+        await page.click(searchSelector);
+        await page.keyboard.type(city);
+        await page.keyboard.press("Enter");
+
+        await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+        await page.waitForSelector('a.lg\\:text-lg.text-black.hover\\:text-black.font-semibold.card-link.mb-1.line-clamp-4', { timeout: 60000 });
+        await page.click('a.lg\\:text-lg.text-black.hover\\:text-black.font-semibold.card-link.mb-1.line-clamp-4');
+        console.log("Clicked on the search result link.");
+        console.log("Navigated URL.");
+
+        try {
+            await page.waitForSelector("a.card-link.line-clamp-2", { timeout: 60000 });
+        } catch (error) {
+            console.error("Error waiting for selector:", error);
+            await browser.close();
+            process.exit(1);
+        }
 
         const links = await page.$$eval("a.card-link.line-clamp-2", (elements) => elements.map(el => el.href));
 
@@ -84,7 +111,7 @@ const cachePath = path.join(__dirname, "cache", cacheFile);
         console.log("Cache saved.");
 
         await browser.close();
-        Console.log("Browser closed.")
+        console.log("Browser closed.")
 
         const endTime = performance.now();
         const endCPU = process.cpuUsage(startCPU);
